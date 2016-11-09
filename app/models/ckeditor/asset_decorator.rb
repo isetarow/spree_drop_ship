1081d4b2a41e7d8f8e08f5d33ba78dfe7ce94f21
yoshida_i <adgj.mptw943@gmail.com>
2016-11-09 15:26:26 +0900

    for totonou

--- app/models/ckeditor/asset_decorator.rb
+++ app/models/ckeditor/asset_decorator.rb
@@ -1,5 +1,5 @@
-if defined?(Ckeditor)
-  Ckeditor::Asset.class_eval do
-    belongs_to :supplier, class_name: 'Spree::Supplier'
-  end
-end
+# if defined?(Ckeditor)
+#   Ckeditor::Asset.class_eval do
+#     belongs_to :supplier, class_name: 'Spree::Supplier'
+#   end
+# end
